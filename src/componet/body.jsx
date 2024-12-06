c45c9ce06f26d4b92250426660bb0888a4954a11
David-Bamgbade <davidbamgbade082@gmail.com>
2024-12-06 18:37:58 +0100

    done signup, caled backend api

--- src/componet/body.jsx
+++ src/componet/body.jsx
@@ -5,6 +5,7 @@ import Card from '../componet/card'
 import Buyer from '../componet/buyer'
 import Seller from '../componet/seller'
 import AboutUs from '../componet/AboutUs'
+import SignUp from "./signUp";
 
 function body() {
   return (
